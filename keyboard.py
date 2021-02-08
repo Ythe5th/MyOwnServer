2bb5a4c195ffbdff1157700b38596eef6fa18411
Ythe3rd <francescoloiacono95@gmail.com>
2021-02-08 18:18:40 +0100

    keyboard ok

--- keyboard.py
+++ keyboard.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     while digit == None:
         digit = kp.getKey()
     # Print result
-    print digit
+    print( digit )
     time.sleep(0.5)
  
     ###### 4 Digit wait ######
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     # Check digit code
     print(seq)
     if seq == [1, 2, 3, '#']:
-        print "Code accepted"
+        print( "Code accepted" )
